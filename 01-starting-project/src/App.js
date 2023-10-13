82000db9b1972fc5551321b3275793959c737ccd
gorav-code <30561844+gorav-code@users.noreply.github.com>
2023-10-13 10:28:54 +0530

    logout related fixes

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,27 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  //check existing login from browser local storage
+  useEffect(() => {
+    const hasUserAlreadyLoggedIn = localStorage.getItem('isLoggedIn');
+    if(hasUserAlreadyLoggedIn === '1'){
+      setIsLoggedIn(true);
+    }
+  }, []);  
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    // But it's just a dummy/ demo anywaysA
+
+    //maintain state using browser local storage
+    localStorage.setItem('isLoggedIn', '1'); 
+    
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn'); 
+
     setIsLoggedIn(false);
   };
 
